7c5e834dfa67e9959a9bf87038a920739257e26d
JustineC31 <justine-cousin@hotmail.com>
2025-01-05 22:05:09 +0100

    Créer ton premier Repository

--- server/src/modules/category/categoryActions.ts
+++ server/src/modules/category/categoryActions.ts
@@ -1,4 +1,5 @@
 import type { RequestHandler } from "express";
+import categoryRepository from "./categoryRepository";
 
 const categories = [
   {
@@ -11,8 +12,10 @@ const categories = [
   },
 ];
 /* Here you code */
-const browse: RequestHandler = (_, res) => {
-  res.json(categories);
+const browse: RequestHandler = async (req, res) => {
+  const categoriesFromDB = await categoryRepository.readAll();
+
+  res.json(categoriesFromDB);
 };
 
 const read: RequestHandler = (req, res) => {
